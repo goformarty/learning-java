087dd75cfa00e18b5717d6fe34408e5e5d1c3e38
Marty Stepien <marty.stepien@gmail.com>
2018-07-03 12:06:17 +0100

    first chapter exercies

---
+++ headfirst/exercises/Dog.java
@@ -0,0 +1,6 @@
+public class Dog {
+	public static void main (String[] args) {
+		System.out.print("Bow wow");
+		System.out.print("Hau");
+	}
+}
---
+++ hello-java/Numbers.java
@@ -0,0 +1,19 @@
+public class Numbers {
+	public static void main(String[] args){
+		byte smallest = 127;
+		short sosmall = -32768;
+		int sonormal = 2000000;
+		long solong = 2000000000;
+		float f = 0.057034583094583707f;
+		float f2 = (float) 0.05;
+		double d =3333333333.99214297658279827364938746298374698273699876987698769876987698769876987698769876;
+		boolean b = true;
+		System.out.println(smallest);
+		System.out.println(sosmall);
+		System.out.println(sonormal);
+		System.out.println(solong);
+		System.out.println(f);
+		System.out.println(f2);
+		System.out.println(d);
+	}
+}
