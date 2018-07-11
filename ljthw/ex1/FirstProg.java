4f7b846303d6cbcefc8f1f9936d23137cd813d5e
Marty Stepien <marty.stepien@gmail.com>
2018-07-11 10:34:32 +0100

    exercise 1

--- ljthw/ex1/FirstProg.java
+++ ljthw/ex1/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg{
 	public static void main(String[] args) {
-		System.out.println("learning java here");
-		System.out.println("Today's date is");
+		System.out.println("Marty's learning java here");
+		System.out.println("Today's date is 11 July 2018");
 	}
 }
